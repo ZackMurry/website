49d8a5a44ed9761faf702da4eef70112ce64f7d9
Zack Murry <zackk.murryy@gmail.com>
2024-02-18 23:17:00 -0600

    Add contact info and Docs Hotkey to index

--- components/ProfileSection.tsx
+++ components/ProfileSection.tsx
@@ -1,9 +1,29 @@
-import { Box, Flex, Heading, Stack, Text, useBreakpointValue } from '@chakra-ui/react'
-import { FC } from 'react'
+import { Box, Flex, Heading, Icon, Stack, Text, useBreakpointValue } from '@chakra-ui/react'
+import { FC, ReactElement } from 'react'
 import Image from 'next/image'
+import { EmailIcon } from '@chakra-ui/icons'
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
+
+interface ContactLinkProps {
+  link: string
+  mt: string
+  text: string
+  icon: any
+}
+
+const ContactLink: FC<ContactLinkProps> = ({ link, mt, text, icon }) => (
+  <a target='_blank' rel='noopener noreferrer' href={link}>
+    <Flex alignItems='center' justifyContent='flex-start'>
+      <Icon as={icon} mt={mt} mr='10px' />
+      <Text mt='5px'>{text}</Text>
+    </Flex>
+  </a>
+)
 
 const ProfileSection: FC = () => {
   const imageSize = useBreakpointValue({ base: 80, xl: 160 })
+  const showContact = useBreakpointValue({ base: false, xl: true })
   return (
     <Stack
       direction={{ base: 'row', xl: 'column' }}
@@ -32,6 +52,13 @@ const ProfileSection: FC = () => {
         <Text mt='5px'>B.S. student in Computer Science and Mathematics</Text>
         <Text mt={{ base: '0px', xl: '5px' }}>University of Missouri</Text>
       </Flex>
+      {showContact && (
+        <Box w='100%'>
+          <ContactLink icon={EmailIcon} mt='7px' link='mailto:zjmfrr@missouri.edu' text='Email' />
+          <ContactLink icon={FaGithub} mt='7px' link='https://github.com/ZackMurry' text='GitHub' />
+          <ContactLink icon={FaLinkedin} mt='5px' link='https://linkedin.com/in/zack-murry' text='LinkedIn' />
+        </Box>
+      )}
     </Stack>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Stack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Flex, Heading, Stack, Text, useBreakpointValue, useColorModeValue } from '@chakra-ui/react'
 import Header from 'components/Header'
 import { FC } from 'react'
 import ProfileSection from 'components/ProfileSection'
@@ -20,130 +20,172 @@ const AboutSection: FC = () => (
   </Box>
 )
 
-const ResearchSection: FC = () => (
-  <Box mt='50px' mx={{ base: 'auto', xl: '0px' }}>
-    <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
-      Research
-    </Heading>
-    <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
-      My work in the VIMAN lab is mostly on designing and executing experiments on the NSF-funded Aerial Experimentation and
-      Research Platform for Advanced Wireless (
-      <a target='_blank' rel='noopener noreferrer' href='https://aerpaw.org/' style={{ textDecoration: 'underline' }}>
-        AERPAW
-      </a>
-      ) testbed, which facilitates experiments with autonomous drones and 5G networks. Specifically, I study computational
-      offloading and control networking for video transmission applications, as well as truck-drone package delivery systems.
-    </Text>
+const ResearchSection: FC = () => {
+  const imgWidth = useBreakpointValue({ base: '33%', lg: '30%', xl: '25%' })
+  return (
+    <Box mt='50px' mx={{ base: 'auto', xl: '0px' }}>
+      <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
+        Research
+      </Heading>
+      <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
+        My work in the VIMAN lab is mostly on designing and executing experiments on the NSF-funded Aerial Experimentation
+        and Research Platform for Advanced Wireless (
+        <a target='_blank' rel='noopener noreferrer' href='https://aerpaw.org/' style={{ textDecoration: 'underline' }}>
+          AERPAW
+        </a>
+        ) testbed, which facilitates experiments with autonomous drones and 5G networks. Specifically, I study computational
+        offloading and control networking for video transmission applications, as well as truck-drone package delivery
+        systems.
+      </Text>
 
-    <Stack direction={{ base: 'column', lg: 'row' }} align='center' justify='center' my='15px'>
-      {/* todo: captions */}
-      <Image
-        src='/qgroundcontrol.jpg'
-        width='632'
-        height='552'
-        alt='Drone navigation screenshot.'
-        style={{ width: 200, height: 200 }}
-      />
-      <Image
-        src='/coral.jpg'
-        width='732'
-        height='732'
-        alt='Google Coral environmental system connected to a Raspberry Pi.'
-        style={{ width: 200, height: 200 }}
-      />
-      <Image
-        src='/CNERT_fig3b.png'
-        width='556'
-        height='445'
-        alt='A figure I made for a paper.'
-        style={{ width: 230, height: 200 }}
-      />
-    </Stack>
-
-    <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
-      Additionally, I have recently started a project in edge computing by using the Internet Equity Initiative's{' '}
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://internetequity.org/floto/'
-        style={{ textDecoration: 'underline' }}
+      <Stack
+        //direction={{ base: 'column', md: 'row' }}
+        direction='row'
+        align='center'
+        justify='center'
+        my='15px'
       >
-        FLOTO
-      </a>{' '}
-      infrastructure for managing experiments in contexts outside of broadband speed/reliability. We are prototyping the use
-      of Google Coral environmental sensors to collect data about the air quality, weather, and more in a distributed manner
-      using Raspberry Pis.
-    </Text>
-  </Box>
-)
+        {/* todo: captions */}
+        <Image
+          src='/qgroundcontrol.jpg'
+          width='632'
+          height='552'
+          alt='Drone navigation screenshot.'
+          style={{ width: imgWidth, aspectRatio: 1 }}
+        />
+        <Image
+          src='/coral.jpg'
+          width='732'
+          height='732'
+          alt='Google Coral environmental system connected to a Raspberry Pi.'
+          style={{ width: imgWidth, aspectRatio: 1 }}
+        />
+        <Image
+          src='/CNERT_fig3b.png'
+          width='556'
+          height='445'
+          alt='A figure I made for a paper.'
+          style={{ width: imgWidth, aspectRatio: 1 }}
+        />
+      </Stack>
 
-const WebDevelopmentSection: FC = () => (
-  <Box mt='50px' mx={{ base: 'auto', xl: '0px' }}>
-    <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
-      Web Development
-    </Heading>
-    <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px' mb='35px'>
-      I started making websites in my freshman year of high school, which was at the start of the pandemic. By following a
-      few free, online courses, I taught myself the basics of the Java Spring framework and React. Since then, I have
-      continued learning through creating projects. A few highlights are listed below, with more projects (both web and
-      non-web) and information on the{' '}
-      <a href='/projects' style={{ textDecoration: 'underline' }}>
-        Projects
-      </a>{' '}
-      page.
-    </Text>
-    <Image
-      src='/chessrs.png'
-      width='1918'
-      height='976'
-      alt='Screenshot from chess project.'
-      style={{ width: '100%', marginTop: '15px' }}
-    />
-    <Text
-      w={{ base: '90%', xl: '80%' }}
-      color='#aaa'
-      textAlign='center'
-      mx='auto'
-      fontSize={{ base: '12px', xl: '14px' }}
-      mb='35px'
-    >
-      ChesSRS is an interactive chess learning website that uses a flashcard-like system for both creating and practicing
-      opening moves.
-    </Text>
+      <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
+        Additionally, I have recently started a project in edge computing by using the Internet Equity Initiative's{' '}
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://internetequity.org/floto/'
+          style={{ textDecoration: 'underline' }}
+        >
+          FLOTO
+        </a>{' '}
+        infrastructure for managing experiments in contexts outside of broadband speed/reliability. We are prototyping the
+        use of Google Coral environmental sensors to collect data about the air quality, weather, and more in a distributed
+        manner using Raspberry Pis.
+      </Text>
+    </Box>
+  )
+}
 
-    {/* docs hotkey */}
-
-    <Image
-      src='/sigma_scoring.png'
-      width='1918'
-      height='976'
-      alt='Screenshot from scorekeeping project.'
-      style={{ width: '100%', marginTop: '15px' }}
-    />
-    {/* todo: change text color based on color mode */}
-    <Text
-      w={{ base: '90%', xl: '80%' }}
-      color='#aaa'
-      textAlign='center'
-      mx='auto'
-      fontSize={{ base: '12px', xl: '14px' }}
-      mb='35px'
-      mt='5px'
-    >
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://central-robotics.github.io/sigma-scoring/'
-        style={{ textDecoration: 'underline' }}
+const WebDevelopmentSection: FC = () => {
+  const captionColor = useColorModeValue('#444', '#aaa')
+  return (
+    <Box mt='50px' mx={{ base: 'auto', xl: '0px' }}>
+      <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
+        Web Development
+      </Heading>
+      <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px' mb='35px'>
+        I started making websites in my freshman year of high school, which was at the start of the pandemic. By following a
+        few free, online courses, I taught myself the basics of the Java Spring framework and React. Since then, I have
+        continued learning through creating projects. A few highlights are listed below, with more projects (both web and
+        non-web) and information on the{' '}
+        <a href='/projects' style={{ textDecoration: 'underline' }}>
+          Projects
+        </a>{' '}
+        page.
+      </Text>
+      <Image
+        src='/chessrs.png'
+        width='1918'
+        height='976'
+        alt='Screenshot from chess project.'
+        style={{ width: '100%', marginTop: '15px' }}
+      />
+      <Text
+        w={{ base: '90%', xl: '80%' }}
+        color={captionColor}
+        textAlign='center'
+        mx='auto'
+        fontSize={{ base: '12px', xl: '14px' }}
+        mb='35px'
+        mt='5px'
       >
-        Sigma Scoring
-      </a>{' '}
-      is a web app that allows for visual scorekeeping for the 2022-2023 FIRST Technology Challenge (FTC) game. I was a
-      programmer for an FTC robotics team for two years in high school, and I currently volunteer at local tournaments to
-      help score matches.
-    </Text>
-  </Box>
-)
+        ChesSRS is an interactive chess learning website that uses a flashcard-like spaced-repetition system (SRS) for
+        practicing opening moves.
+      </Text>
+
+      {/* docs hotkey */}
+      <Image
+        src='/docs_hotkey_cws.png'
+        // width='1918'
+        // height='976'
+        width='1493'
+        height='702'
+        alt='Docs Hotkey Chrome Webstore Page.'
+        style={{ width: '100%', marginTop: '15px' }}
+      />
+      <Text
+        w={{ base: '90%', xl: '80%' }}
+        color={captionColor}
+        textAlign='center'
+        mx='auto'
+        fontSize={{ base: '12px', xl: '14px' }}
+        mb='35px'
+        mt='5px'
+      >
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://docs-hotkey.zackmurry.com/'
+          style={{ textDecoration: 'underline' }}
+        >
+          Docs Hotkey
+        </a>{' '}
+        is a Chrome extension for Google Docs that allows the creation of complex shortcuts. It has over 2,500 weekly users
+        and a 4.9-star rating.
+      </Text>
+
+      <Image
+        src='/sigma_scoring.png'
+        width='1918'
+        height='976'
+        alt='Screenshot from scorekeeping project.'
+        style={{ width: '100%', marginTop: '15px' }}
+      />
+      <Text
+        w={{ base: '90%', xl: '80%' }}
+        color={captionColor}
+        textAlign='center'
+        mx='auto'
+        fontSize={{ base: '12px', xl: '14px' }}
+        mb='35px'
+        mt='5px'
+      >
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://central-robotics.github.io/sigma-scoring/'
+          style={{ textDecoration: 'underline' }}
+        >
+          Sigma Scoring
+        </a>{' '}
+        is a web app that allows for visual scorekeeping for the 2022-2023 FIRST Technology Challenge (FTC) game. I was a
+        programmer for my high school's FTC robotics team for two years, and I currently volunteer at local tournaments to
+        help score matches.
+      </Text>
+    </Box>
+  )
+}
 
 // todo: extract this code into _app.tsx
 const HomePage: FC = () => (
