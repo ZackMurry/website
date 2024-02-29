61d085b1026b73a9ea9f0b9871285d36db5cdb76
Zack Murry <zackk.murryy@gmail.com>
2024-02-29 14:38:41 -0600

    Add CV and improve responsiveness

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,5 +1,5 @@
-import { extendTheme } from '@chakra-ui/react'
-import { mode } from '@chakra-ui/theme-tools'
+import {extendTheme} from '@chakra-ui/react'
+import {mode} from '@chakra-ui/theme-tools'
 
 const theme = extendTheme({
   styles: {
@@ -7,26 +7,26 @@ const theme = extendTheme({
       'body, html': {
         backgroundColor: mode('#eeeeee', '#181a1b')(props),
         color: mode('#000000', '#e8e6e3')(props),
-        scrollBehavior: 'smooth'
-      }
-    })
+        scrollBehavior: 'smooth',
+      },
+    }),
   },
   components: {
     Heading: {
       baseStyle: {
-        fontFamily: '"Open Sans", Calibri, serif, sans-serif'
-      }
+        fontFamily: '"Open Sans", Calibri, serif, sans-serif',
+      },
     },
     Text: {
       baseStyle: {
         fontFamily:
-          '"Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"'
-      }
-    }
+          '"Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+      },
+    },
   },
   config: {
-    initialColorMode: 'dark'
-  }
+    initialColorMode: 'light',
+  },
 })
 
 export default theme
