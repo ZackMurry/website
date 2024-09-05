c3536e2bf5e6c5920af90ddb200c6377e41255c7
Zack Murry <zackk.murryy@gmail.com>
2024-09-05 18:41:41 -0500

    Update website with internship etc

--- components/ProfileSection.tsx
+++ components/ProfileSection.tsx
@@ -7,11 +7,12 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react'
-import {FC, ReactElement} from 'react'
+import { FC, ReactElement } from 'react'
 import Image from 'next/image'
-import {EmailIcon} from '@chakra-ui/icons'
-import {FaLinkedin} from '@react-icons/all-files/fa/FaLinkedin'
-import {FaGithub} from '@react-icons/all-files/fa/FaGithub'
+import { EmailIcon } from '@chakra-ui/icons'
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
+import { FaGoogleScholar } from 'react-icons/fa6'
 
 interface ContactLinkProps {
   link: string
@@ -20,70 +21,78 @@ interface ContactLinkProps {
   icon: any
 }
 
-const ContactLink: FC<ContactLinkProps> = ({link, mt, text, icon}) => (
-  <a target="_blank" rel="noopener noreferrer" href={link}>
-    <Flex alignItems="center" justifyContent="flex-start">
-      <Icon as={icon} mt={mt} mr="10px" />
-      <Text mt="5px">{text}</Text>
+const ContactLink: FC<ContactLinkProps> = ({ link, mt, text, icon }) => (
+  <a target='_blank' rel='noopener noreferrer' href={link}>
+    <Flex alignItems='center' justifyContent='flex-start'>
+      <Icon as={icon} mt={mt} mr='10px' />
+      <Text mt='5px'>{text}</Text>
     </Flex>
   </a>
 )
 
 const ProfileSection: FC = () => {
-  const imageSize = useBreakpointValue({base: 80, xl: 160})
-  const showContact = useBreakpointValue({base: false, xl: true})
+  const imageSize = useBreakpointValue({ base: 80, xl: 160 })
+  const showContact = useBreakpointValue({ base: false, xl: true })
   return (
     <Stack
-      direction={{base: 'row', xl: 'column'}}
-      justify="flex-start"
-      align="center"
-      alignSelf="baseline"
-      position={{base: undefined, xl: 'sticky'}}
-      w={{base: '90%', xl: '20%'}}
-      top="5em"
-      m={{base: '0 auto', xl: '0 30px 0 0'}}>
-      <Box mx={{base: '0', xl: 'auto'}}>
+      direction={{ base: 'row', xl: 'column' }}
+      justify='flex-start'
+      align='center'
+      alignSelf='baseline'
+      position={{ base: undefined, xl: 'sticky' }}
+      w={{ base: '90%', xl: '20%' }}
+      top='5em'
+      m={{ base: '0 auto', xl: '0 30px 0 0' }}
+    >
+      <Box mx={{ base: '0', xl: 'auto' }}>
         <Image
-          src="/profile_pic_cropped.png"
+          src='/headshot.jpg'
           // src=''
           width={imageSize}
           height={imageSize}
-          alt="Profile picture."
-          style={{borderRadius: '50%'}}
+          alt='Profile picture.'
+          style={{ borderRadius: '50%' }}
         />
       </Box>
-      <Flex flexDir="column" justifyContent="center" py="15px">
-        <Heading as="h6" fontSize={{base: '18px', xl: '22px'}}>
+      <Flex flexDir='column' justifyContent='center' py='15px'>
+        <Heading as='h6' fontSize={{ base: '18px', xl: '22px' }}>
           Zack Murry
         </Heading>
-        <Text mt="5px" fontSize={{base: '14px', xl: '16px'}}>
+        <Text mt='5px' fontSize={{ base: '14px', xl: '16px' }}>
           B.S. student in Computer Science and Mathematics
         </Text>
         <Text
-          mt={{base: '0px', xl: '5px'}}
-          fontSize={{base: '14px', xl: '16px'}}>
+          mt={{ base: '0px', xl: '5px' }}
+          fontSize={{ base: '14px', xl: '16px' }}
+        >
           University of Missouri
         </Text>
       </Flex>
       {showContact && (
-        <Box w="100%">
+        <Box w='100%'>
           <ContactLink
             icon={EmailIcon}
-            mt="7px"
-            link="mailto:zjmfrr@missouri.edu"
-            text="Email"
+            mt='7px'
+            link='mailto:zjmfrr@missouri.edu'
+            text='Email'
           />
           <ContactLink
             icon={FaGithub}
-            mt="7px"
-            link="https://github.com/ZackMurry"
-            text="GitHub"
+            mt='7px'
+            link='https://github.com/ZackMurry'
+            text='GitHub'
           />
           <ContactLink
             icon={FaLinkedin}
-            mt="5px"
-            link="https://linkedin.com/in/zack-murry"
-            text="LinkedIn"
+            mt='5px'
+            link='https://linkedin.com/in/zack-murry'
+            text='LinkedIn'
+          />
+          <ContactLink
+            icon={FaGoogleScholar}
+            mt='5px'
+            link='https://scholar.google.com/citations?user=ZZBezfYAAAAJ'
+            text='Google Scholar'
           />
         </Box>
       )}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,95 +8,133 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import Header from 'components/Header'
-import {FC} from 'react'
+import { FC } from 'react'
 import ProfileSection from 'components/ProfileSection'
-import {ExternalLinkIcon} from '@chakra-ui/icons'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import Image from 'next/image'
 
 const AboutSection: FC = () => (
-  <Box mx={{base: 'auto', xl: '0px'}}>
-    <Heading as="h3" fontWeight="600" fontSize={{base: '20px', xl: '28px'}}>
+  <Box mx={{ base: 'auto', xl: '0px' }}>
+    <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
       About
     </Heading>
-    <Text fontSize={{base: '14px', xl: '18px'}} mt="15px">
-      Hi! I'm Zack, a first-year undergraduate student in Computer Science and
+    <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
+      Hi! I'm Zack, a second-year undergraduate student in Computer Science and
       Mathematics at the University of Missouri. I work as a student research
       assistant in the Virtualization, Multimedia, and Networking (VIMAN) lab,
-      where I study drone networks and edge computing. I also enjoy front- and
-      back-end web development, creating compilers, and working with robots!
-      {/* todo: introduce webdev and compiler work, maybe in next section though */}
+      where I study computer networking, drone navigation, and edge computing.
+      This past summer (2024), I did an internship at the University of Chicago
+      researching distributed computing using Raspberry Pis. Outside of
+      research, I also enjoy full-stack web development, compilers, and robots!
     </Text>
   </Box>
 )
 
 const ResearchSection: FC = () => {
-  const imgWidth = useBreakpointValue({base: '33%', lg: '30%', xl: '25%'})
+  const imgWidth = useBreakpointValue({ base: '33%', lg: '30%', xl: '25%' })
   return (
-    <Box mt="50px" mx={{base: 'auto', xl: '0px'}}>
-      <Heading as="h3" fontWeight="600" fontSize={{base: '20px', xl: '28px'}}>
+    <Box mt='50px' mx={{ base: 'auto', xl: '0px' }}>
+      <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
         Research
       </Heading>
-      <Text fontSize={{base: '14px', xl: '18px'}} mt="15px">
-        My work in the VIMAN lab is mostly on designing and executing
-        experiments on the NSF-funded Aerial Experimentation and Research
+      <Heading
+        as='h4'
+        fontWeight='600'
+        fontSize={{ base: '16px', xl: '20px' }}
+        fontStyle='italic'
+        color='gray.600'
+        mt='15px'
+      >
+        VIMAN
+      </Heading>
+      <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
+        My work in the VIMAN lab is focused on creating solutions for autonomous
+        drones. For example, we're currently working on a route generation
+        software for package delivery using drones and delivery trucks via
+        Reinforcement Learning (RL) with a Deep Q-Network (DQN). Additionally, I
+        run experiments on the NSF-funded Aerial Experimentation and Research
         Platform for Advanced Wireless (
         <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://aerpaw.org/"
-          style={{textDecoration: 'underline'}}>
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://aerpaw.org/'
+          style={{ textDecoration: 'underline' }}
+        >
           AERPAW
         </a>
         ) testbed, which facilitates experiments with autonomous drones and 5G
         networks. Specifically, I study computational offloading and control
         networking for video transmission applications, as well as truck-drone
-        package delivery systems.
+        package delivery systems. We published a paper at an IEEE workshop
+        earlier this year with experiments on the AERPAW testbed, which is
+        available{' '}
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://ieeexplore.ieee.org/abstract/document/10620812/'
+          style={{ textDecoration: 'underline' }}
+        >
+          here
+        </a>
+        .
       </Text>
 
       <Stack
         //direction={{ base: 'column', md: 'row' }}
-        direction="row"
-        align="center"
-        justify="center"
-        my="15px">
+        direction='row'
+        align='center'
+        justify='center'
+        my='15px'
+      >
         {/* todo: captions */}
         <Image
-          src="/qgroundcontrol.jpg"
-          width="632"
-          height="552"
-          alt="Drone navigation screenshot."
-          style={{width: imgWidth, aspectRatio: 1}}
+          src='/qgroundcontrol.jpg'
+          width='632'
+          height='552'
+          alt='Drone navigation screenshot.'
+          style={{ width: imgWidth, aspectRatio: 1 }}
         />
         <Image
-          src="/coral.jpg"
-          width="732"
-          height="732"
-          alt="Google Coral environmental system connected to a Raspberry Pi."
-          style={{width: imgWidth, aspectRatio: 1}}
+          src='/coral.jpg'
+          width='732'
+          height='732'
+          alt='Google Coral environmental system connected to a Raspberry Pi.'
+          style={{ width: imgWidth, aspectRatio: 1 }}
         />
         <Image
-          src="/CNERT_fig3b.png"
-          width="556"
-          height="445"
-          alt="A figure I made for a paper."
-          style={{width: imgWidth, aspectRatio: 1}}
+          src='/CNERT_fig3b.png'
+          width='556'
+          height='445'
+          alt='A figure I made for a paper.'
+          style={{ width: imgWidth, aspectRatio: 1 }}
         />
       </Stack>
 
-      <Text fontSize={{base: '14px', xl: '18px'}} mt="15px">
-        Additionally, I have recently started a project in edge computing by
-        using the Internet Equity Initiative's{' '}
+      <Heading
+        as='h4'
+        fontWeight='600'
+        fontSize={{ base: '16px', xl: '20px' }}
+        fontStyle='italic'
+        color='gray.600'
+        mt='30px'
+      >
+        University of Chicago
+      </Heading>
+      <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px'>
+        At my internship, I collaborated with the Agricultural and Rural (
         <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://internetequity.org/floto/"
-          style={{textDecoration: 'underline'}}>
-          FLOTO
-        </a>{' '}
-        infrastructure for managing experiments in contexts outside of broadband
-        speed/reliability. We are prototyping the use of Google Coral
-        environmental sensors to collect data about the air quality, weather,
-        and more in a distributed manner using Raspberry Pis.
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://aerpaw.org/'
+          style={{ textDecoration: 'underline' }}
+        >
+          ARA
+        </a>
+        ) Wireless Living Lab to deploy six Raspberry Pis in and around Ames,
+        Iowa across farms and Iowa State buildings. These devices are connected
+        to ARA's 5G radio infrastructure, and I conducted distributed computing
+        experiments across these 5G links using Apache Hadoop to run MapReduce
+        workloads.
       </Text>
     </Box>
   )
@@ -105,37 +143,38 @@ const ResearchSection: FC = () => {
 const WebDevelopmentSection: FC = () => {
   const captionColor = useColorModeValue('#444', '#aaa')
   return (
-    <Box mt="50px" mx={{base: 'auto', xl: '0px'}}>
-      <Heading as="h3" fontWeight="600" fontSize={{base: '20px', xl: '28px'}}>
+    <Box mt='50px' mx={{ base: 'auto', xl: '0px' }}>
+      <Heading as='h3' fontWeight='600' fontSize={{ base: '20px', xl: '28px' }}>
         Web Development
       </Heading>
-      <Text fontSize={{base: '14px', xl: '18px'}} mt="15px" mb="35px">
+      <Text fontSize={{ base: '14px', xl: '18px' }} mt='15px' mb='35px'>
         I started making websites in my freshman year of high school, which was
         at the start of the pandemic. By following a few free, online courses, I
         taught myself the basics of the Java Spring framework and React. Since
         then, I have continued learning through creating projects. A few
         highlights are listed below, with more projects (both web and non-web)
         and information on the{' '}
-        <a href="/projects.html" style={{textDecoration: 'underline'}}>
+        <a href='/projects.html' style={{ textDecoration: 'underline' }}>
           Projects
         </a>{' '}
         page.
       </Text>
       <Image
-        src="/chessrs.png"
-        width="1918"
-        height="976"
-        alt="Screenshot from chess project."
-        style={{width: '100%', marginTop: '15px'}}
+        src='/chessrs.png'
+        width='1918'
+        height='976'
+        alt='Screenshot from chess project.'
+        style={{ width: '100%', marginTop: '15px' }}
       />
       <Text
-        w={{base: '90%', xl: '80%'}}
+        w={{ base: '90%', xl: '80%' }}
         color={captionColor}
-        textAlign="center"
-        mx="auto"
-        fontSize={{base: '12px', xl: '14px'}}
-        mb="35px"
-        mt="5px">
+        textAlign='center'
+        mx='auto'
+        fontSize={{ base: '12px', xl: '14px' }}
+        mb='35px'
+        mt='5px'
+      >
         ChesSRS is an interactive chess learning website that uses a
         flashcard-like spaced-repetition system (SRS) for practicing opening
         moves.
@@ -143,53 +182,57 @@ const WebDevelopmentSection: FC = () => {
 
       {/* docs hotkey */}
       <Image
-        src="/docs_hotkey_cws.png"
+        src='/docs_hotkey_cws.png'
         // width='1918'
         // height='976'
-        width="1493"
-        height="702"
-        alt="Docs Hotkey Chrome Webstore Page."
-        style={{width: '100%', marginTop: '15px'}}
+        width='1493'
+        height='702'
+        alt='Docs Hotkey Chrome Webstore Page.'
+        style={{ width: '100%', marginTop: '15px' }}
       />
       <Text
-        w={{base: '90%', xl: '80%'}}
+        w={{ base: '90%', xl: '80%' }}
         color={captionColor}
-        textAlign="center"
-        mx="auto"
-        fontSize={{base: '12px', xl: '14px'}}
-        mb="35px"
-        mt="5px">
+        textAlign='center'
+        mx='auto'
+        fontSize={{ base: '12px', xl: '14px' }}
+        mb='35px'
+        mt='5px'
+      >
         <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://docs-hotkey.zackmurry.com/"
-          style={{textDecoration: 'underline'}}>
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://docs-hotkey.zackmurry.com/'
+          style={{ textDecoration: 'underline' }}
+        >
           Docs Hotkey
         </a>{' '}
         is a Chrome extension for Google Docs that allows the creation of
-        complex shortcuts. It has over 2,500 weekly users and a 4.9-star rating.
+        complex shortcuts. It has over 4,500 weekly users and a 4.9-star rating.
       </Text>
 
       <Image
-        src="/sigma_scoring.png"
-        width="1918"
-        height="976"
-        alt="Screenshot from scorekeeping project."
-        style={{width: '100%', marginTop: '15px'}}
+        src='/sigma_scoring.png'
+        width='1918'
+        height='976'
+        alt='Screenshot from scorekeeping project.'
+        style={{ width: '100%', marginTop: '15px' }}
       />
       <Text
-        w={{base: '90%', xl: '80%'}}
+        w={{ base: '90%', xl: '80%' }}
         color={captionColor}
-        textAlign="center"
-        mx="auto"
-        fontSize={{base: '12px', xl: '14px'}}
-        mb="35px"
-        mt="5px">
+        textAlign='center'
+        mx='auto'
+        fontSize={{ base: '12px', xl: '14px' }}
+        mb='35px'
+        mt='5px'
+      >
         <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://central-robotics.github.io/sigma-scoring/"
-          style={{textDecoration: 'underline'}}>
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://central-robotics.github.io/sigma-scoring/'
+          style={{ textDecoration: 'underline' }}
+        >
           Sigma Scoring
         </a>{' '}
         is a web app that allows for visual scorekeeping for the 2022-2023 FIRST
@@ -203,18 +246,19 @@ const WebDevelopmentSection: FC = () => {
 
 // todo: extract this code into _app.tsx
 const HomePage: FC = () => (
-  <Box mx={{base: '0%', lg: '5%', xl: '10%'}}>
+  <Box mx={{ base: '0%', lg: '5%', xl: '10%' }}>
     <Header />
     <Stack
       //className={styles.content} */}
-      direction={{base: 'column', xl: 'row'}}
-      mx="auto"
-      maxWidth="1080px"
-      width={{base: '95%', md: '85%', lg: '80%', xl: '65%'}}
-      mt={{base: '20px', xl: '50px'}}
-      mb="350px">
+      direction={{ base: 'column', xl: 'row' }}
+      mx='auto'
+      maxWidth='1080px'
+      width={{ base: '95%', md: '85%', lg: '80%', xl: '65%' }}
+      mt={{ base: '20px', xl: '50px' }}
+      mb='350px'
+    >
       <ProfileSection />
-      <Box w={{base: '100%', lg: '90%', xl: '80%'}} mx="auto">
+      <Box w={{ base: '100%', lg: '90%', xl: '80%' }} mx='auto'>
         <AboutSection />
         <ResearchSection />
         <WebDevelopmentSection />
