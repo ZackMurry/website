e6923694c23bc94bd92779407897c21982e5ec10
Zack Murry <zack.murry@garmin.com>
2025-06-30 23:28:08 -0500

    Update links page

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -20,7 +20,7 @@ const LinksPage = () => (
     <Header />
     <div className='grid gap-0'>
       <h1 className='text-zinc-900 font-bold text-lg'>Links</h1>
-      <h4 className='text-zinc-600 text-sm mt-1'>Here&apos;s my resume, projects, socials, etc.</h4>
+      <h4 className='text-zinc-600 text-sm mt-1'>Here are links to my resume, projects, socials, etc.</h4>
     </div>
     <article className='text-zinc-600 grid gap-4'>
       <ul className='grid gap-2 list-disc list-inside text-zinc-600'>
